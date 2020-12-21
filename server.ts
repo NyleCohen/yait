adc971679fe5cb2d84b34f7a6176c3005b2791f7
Nyle Cohen <nylecohen@gmail.com>
2020-12-21 21:04:38 +0000

    added eslint

--- server.ts
+++ server.ts
@@ -1,12 +1,13 @@
-import express from "express";
-import mongoose from "mongoose";
-import path from "path";
-import bodyParser from "body-parser";
-import { v4 as uuidv4 } from "uuid";
+import express from 'express';
+import mongoose from 'mongoose';
+import path from 'path';
+import bodyParser from 'body-parser';
+import { v4 as uuidv4 } from 'uuid';
 
-const connectDB = require("./config/db");
-const Org = require("./model/createOrg");
-const Issue = require("./model/createIssue");
+const connectDB = require('./config/db');
+const Org = require('./model/createOrg');
+const Issue = require('./model/createIssue');
+
 const app: express.Application = express();
 const port: number = 8080;
 
@@ -16,11 +17,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const orgUuid = uuidv4();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/index.html`));
 });
 
-app.post("/createOrg", async (req, res) => {
+app.post('/createOrg', async (req, res) => {
   const org = new Org({
     org_id: orgUuid,
     name: req.body.orgname,
@@ -38,30 +39,30 @@ app.post("/createOrg", async (req, res) => {
     });
 
   res.status(200);
-  res.redirect("/org/" + orgUuid);
+  res.redirect(`/org/${orgUuid}`);
 });
 
-app.get("/org/:orgId", async (req, res) => {
+app.get('/org/:orgId', async (req, res) => {
   const orgId = await Org.findOne({ org_id: req.params.orgId });
   if (orgId == null) return res.sendStatus(404);
 
-  res.sendFile(path.join(__dirname + "/views/org.html"));
-  //res.json(await Org.findOne({ org_id: req.params.orgId }));
+  res.sendFile(path.join(`${__dirname}/views/org.html`));
+  // res.json(await Org.findOne({ org_id: req.params.orgId }));
 });
 
-app.get("/org/:orgId/orgMeta", async (req, res) => {
+app.get('/org/:orgId/orgMeta', async (req, res) => {
   const orgId = await Org.findOne({ org_id: req.params.orgId });
   if (orgId == null) return res.sendStatus(404);
 
   res.json(await Org.findOne({ org_id: req.params.orgId }));
 });
 
-app.get("/org/:orgId/createIssue", async (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/createissue.html"));
+app.get('/org/:orgId/createIssue', async (req, res) => {
+  res.sendFile(path.join(`${__dirname}/views/createissue.html`));
 });
 
-app.post("/org/:orgId/createIssue", async (req, res) => {
-  const urlOrgId = req.originalUrl.split("/");
+app.post('/org/:orgId/createIssue', async (req, res) => {
+  const urlOrgId = req.originalUrl.split('/');
 
   const issue = new Issue({
     id: uuidv4(),
@@ -83,13 +84,13 @@ app.post("/org/:orgId/createIssue", async (req, res) => {
   res.status(200);
 });
 
-app.get("/org/:orgId/issues", async (req, res) => {
-  const urlOrgId = req.originalUrl.split("/");
-  const issues = await Issue.find({ org_id: urlOrgId }).where("id != 0");
+app.get('/org/:orgId/issues', async (req, res) => {
+  const urlOrgId = req.originalUrl.split('/');
+  const issues = await Issue.find({ org_id: urlOrgId }).where('id != 0');
 
   res.json(issues);
 });
 
 app.listen(port, () => {
-  console.log("Online on port", port);
+  console.log('Online on port', port);
 });
